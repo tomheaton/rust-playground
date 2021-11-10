d343e8dee2ca0f8e82481b08201181123f27c51d
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-10 13:09:53 +0000

    comments :scroll:

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ macro_rules! test_macro {
 
 fn main() {
     println!("Hello World!");
+
+    // testing a macro
     test_macro!();
+
+    // debug format?
     println!("hello, {:?} ?", "testing");
+
+    // change a mutable boolean
+    let mut test_bool: bool = true;
+    test_bool = false;
+
+    // suffix annotation
+    let test_float = 0.32f32;
+    // default annotation
+    let test_float_two: f64 = 0.64;
 }
