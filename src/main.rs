b155661cdfc99cadc777c99e0b186d0a056e6050
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-11 21:09:42 +0000

    learning...

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,13 @@ fn main() {
     let test_float = 0.32f32;
     // default annotation
     let test_float_two: f64 = 0.64;
+
+    let mut my_list: Vec<i32> = vec![23, 65, 34, 88, 75, 9, 12];
+    for i in my_list {
+        println!("{:?}", i);
+    }
+
+    //my_list.push(91);
+
+    let test_list: Vec<i32> = Vec::with_capacity(11);
 }
