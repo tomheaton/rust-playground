04dbf7f0fe4ef61b2706a476655c54598d1b934e
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-06-26 11:50:59 +0100

    docstrings

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -1,3 +1,4 @@
+/// check if a number is divisible by another
 fn is_divisible_by(num: u32, den: u32) -> bool {
     if den == 0 {
         return false;
@@ -5,6 +6,7 @@ fn is_divisible_by(num: u32, den: u32) -> bool {
     num % den == 0
 }
 
+/// fizzbuzz for a number n
 fn fizzbuzz(n: u32) -> () {
     if is_divisible_by(n, 15) {
         println!("fizzbuzz");
@@ -17,6 +19,7 @@ fn fizzbuzz(n: u32) -> () {
     }
 }
 
+/// perform fizzbuzz to a number n
 pub fn fizzbuzz_to(n: u32) {
     for n in 1..=n {
         fizzbuzz(n);
