606b1c3905b1d66941cd41bb8b22cc58d426af5a
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-08-02 23:22:09 +0100

    inputs!

--- src/new.rs
+++ src/new.rs
@@ -1,4 +1,7 @@
 fn main() {
     let mut _line = String::new();
     println!("Enter your name :");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Hello , {}", line);
+    println!("no of bytes read , {}", b1);
 }
